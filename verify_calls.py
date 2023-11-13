27e6cb3cbf815e33d6b736a9e7d0d443328885f7
shalom <shalommitz@gmail.com>
2023-11-13 10:06:03 +0000

    Added PLAY_BACK mode: will play the recoded inputs to the function and verify results

--- verify_calls.py
+++ verify_calls.py
@@ -3,8 +3,8 @@ from deepdiff import DeepDiff
 
 class Verify():
     def __init__(self, mode, folder):
-        if mode not in ["RECORD", "VERIFY"]:
-            print("ERROR: Mode paramter should be RECORD or VERIFY - Aborting")
+        if mode not in ["RECORD", "VERIFY", "PLAY_BACK"]:
+            print("ERROR: Mode paramter should be RECORD or VERIFY or PLAY_BACK - Aborting")
             exit()
         self.mode = mode
         print("mode set to", self.mode)
@@ -63,9 +63,10 @@ class Verify():
             if self.mode=="RECORD":
                 pickle.dump(args, self.get_dump_file(name, "args_befor"))
                 pickle.dump(kwargs, self.get_dump_file(name, "kwargs_befor"))
-            else:
-                #if self.is_compare_failed(name, "args_befor", args): exit()
-                #if self.is_compare_failed(name, "kwargs_befor", kwargs): exit()
+            elif self.mode=="PLAY_BACK": :
+                args = pickle.load(self.get_load_file(name, "args_befor"))
+                kwargs = pickle.load(self.get_load_file(name, "kwargs_befor"))
+            else:     # VERIFY
                 self.is_compare_failed(name, "args_befor", args)
                 self.is_compare_failed(name, "kwargs_befor", kwargs)
             ans = func(*args, **kwargs)
@@ -73,7 +74,7 @@ class Verify():
                 pickle.dump(args, self.get_dump_file(name, "args_after"))
                 pickle.dump(kwargs, self.get_dump_file(name, "kwargs_after"))
                 pickle.dump(ans, self.get_dump_file(name, "ans"))
-            else:
+            else:      # VERIFY or PLAY_BACK
                 #if self.is_compare_failed(name, "args_after", args): exit()
                 #if self.is_compare_failed(name, "kwargs_after", kwargs): exit()
                 #if self.is_compare_failed(name, "ans", ans): exit()
